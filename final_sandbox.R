793ad1a6b938b044a33e94c43a997ea758799870
aj-cooke <62916946+aj-cooke@users.noreply.github.com>
2021-11-26 21:44:52 -0600

    Added squared terms

--- final_sandbox.R
+++ final_sandbox.R
@@ -33,18 +33,18 @@ corrplot(M)
 
 # Individual with new features
 change_ind <- glm.nb(Individual ~ Mean_800 + 
-                        Jewish_segment + JLR_station +
-                        Damascus_Gate_dis + With_settlements +
-                        change_1250_800 + change_2000_1250,
-                      data=jlm_con)
+                       Jewish_segment + JLR_station +
+                       Damascus_Gate_dis + With_settlements +
+                       change_1250_800 + change_2000_1250,
+                     data=jlm_con)
 summary(change_ind)
 
 #Collective with new features
 change_col <- glm.nb(Collective ~ Mean_800 + 
-                        Jewish_segment + JLR_station +
-                        Damascus_Gate_dis + With_settlements +
-                        change_1250_800 + change_2000_1250,
-                      data=jlm_con)
+                       Jewish_segment + JLR_station +
+                       Damascus_Gate_dis + With_settlements +
+                       change_1250_800 + change_2000_1250,
+                     data=jlm_con)
 summary(change_col)
 
 # For zero inflated: https://stats.idre.ucla.edu/r/dae/zinb/ and https://stats.idre.ucla.edu/r/dae/zip/
@@ -85,17 +85,17 @@ jlm_con <- jlm_con %>%
          Collective_ind = ifelse(Collective == 0, 0, 1))
 
 ind_log <- glm(Individual_ind ~ Mean_800 + 
-                       Jewish_segment + JLR_station +
-                       Damascus_Gate_dis + With_settlements +
-                       change_1250_800 + change_2000_1250,
-                     data=jlm_con, family = "binomial")
+                 Jewish_segment + JLR_station +
+                 Damascus_Gate_dis + With_settlements +
+                 change_1250_800 + change_2000_1250,
+               data=jlm_con, family = "binomial")
 summary(ind_log)
 
 col_log <- glm(Collective_ind ~ Mean_800 + 
-                       Jewish_segment + JLR_station +
-                       Damascus_Gate_dis + With_settlements +
-                       change_1250_800 + change_2000_1250,
-                     data=jlm_con, family = "binomial")
+                 Jewish_segment + JLR_station +
+                 Damascus_Gate_dis + With_settlements +
+                 change_1250_800 + change_2000_1250,
+               data=jlm_con, family = "binomial")
 summary(col_log)
 
 # Next, account for class imbalance by upsampling, use F1/specificity/sensitivity
@@ -108,58 +108,81 @@ col_smote <- col_smote$data
 
 # Smoted models
 ind_smote_log <- glm(Individual_ind ~ Mean_800 + 
-                 Jewish_segment + JLR_station +
-                 Damascus_Gate_dis + With_settlements +
-                 change_1250_800 + change_2000_1250,
-               data=ind_smote, family = "binomial")
+                       Jewish_segment + JLR_station +
+                       Damascus_Gate_dis + With_settlements +
+                       change_1250_800 + change_2000_1250,
+                     data=ind_smote, family = "binomial")
 summary(ind_smote_log)
 
 col_smote_log <- glm(Collective_ind ~ Mean_800 + 
-                 Jewish_segment + JLR_station +
-                 Damascus_Gate_dis + With_settlements +
-                 change_1250_800 + change_2000_1250,
-               data=col_smote, family = "binomial")
+                       Jewish_segment + JLR_station +
+                       Damascus_Gate_dis + With_settlements +
+                       change_1250_800 + change_2000_1250,
+                     data=col_smote, family = "binomial")
 summary(col_smote_log)
 
 # Significant only
 ind_smote_tuned <- glm(Individual_ind ~ JLR_station +
-                       Damascus_Gate_dis + With_settlements +
-                       change_2000_1250,
-                     data=ind_smote, family = "binomial")
+                         Damascus_Gate_dis + With_settlements +
+                         change_2000_1250,
+                       data=ind_smote, family = "binomial")
 summary(ind_smote_tuned)
 
 col_smote_tuned <- glm(Collective_ind ~ Jewish_segment +
-                       Damascus_Gate_dis + With_settlements,
-                     data=col_smote, family = "binomial")
+                         Damascus_Gate_dis + With_settlements,
+                       data=col_smote, family = "binomial")
 summary(col_smote_tuned)
 
 # Interactions
 ind_smote_int <- glm(Individual_ind ~ JLR_station +
-                         Damascus_Gate_dis + With_settlements +
-                         change_2000_1250 + JLR_station:Damascus_Gate_dis +
-                      JLR_station:With_settlements + JLR_station:change_2000_1250 +
+                       Damascus_Gate_dis + With_settlements +
+                       change_2000_1250 + JLR_station:Damascus_Gate_dis +
+                       JLR_station:With_settlements + JLR_station:change_2000_1250 +
                        Damascus_Gate_dis:With_settlements + 
                        Damascus_Gate_dis:change_2000_1250 + With_settlements:change_2000_1250,
-                       data=ind_smote, family = "binomial")
+                     data=ind_smote, family = "binomial")
 summary(ind_smote_int)
 
 col_smote_int <- glm(Collective_ind ~ Jewish_segment +
-                         Damascus_Gate_dis + With_settlements +
+                       Damascus_Gate_dis + With_settlements +
                        Jewish_segment:Damascus_Gate_dis + 
                        Jewish_segment:With_settlements +
                        Damascus_Gate_dis:With_settlements,
-                       data=col_smote, family = "binomial")
+                     data=col_smote, family = "binomial")
 summary(col_smote_int)
 
 # Interactions tuned
 ind_smote_int_tuned <- glm(Individual_ind ~ JLR_station +
-                       Damascus_Gate_dis + With_settlements +
-                       change_2000_1250 +
-                       With_settlements:change_2000_1250,
-                     data=ind_smote, family = "binomial")
+                             Damascus_Gate_dis + With_settlements +
+                             change_2000_1250 +
+                             With_settlements:change_2000_1250,
+                           data=ind_smote, family = "binomial")
 summary(ind_smote_int_tuned)
 
 col_smote_int_tuned <- glm(Collective_ind ~ Jewish_segment +
-                       Damascus_Gate_dis + Jewish_segment:Damascus_Gate_dis,
-                     data=col_smote, family = "binomial")
+                             Damascus_Gate_dis + Jewish_segment:Damascus_Gate_dis,
+                           data=col_smote, family = "binomial")
 summary(col_smote_int_tuned)
+
+#squared terms (unsuccessful)
+ind_smote <- ind_smote %>%
+  mutate(Damascus_sq = Damascus_Gate_dis^2,
+         change_2k_sq = change_2000_1250^2)
+
+col_smote <- col_smote %>%
+  mutate(Damascus_sq = Damascus_Gate_dis^2,
+         change_2k_sq = change_2000_1250^2)
+
+ind_smote_int_sq <- glm(Individual_ind ~ JLR_station +
+                             Damascus_Gate_dis + With_settlements +
+                             change_2000_1250 +
+                             With_settlements:change_2000_1250 +
+                          Damascus_sq + change_2k_sq,
+                           data=ind_smote, family = "binomial")
+summary(ind_smote_int_sq)
+
+col_smote_int_sq <- glm(Collective_ind ~ Jewish_segment +
+                             Damascus_Gate_dis + Jewish_segment:Damascus_Gate_dis +
+                          Damascus_sq,
+                           data=col_smote, family = "binomial")
+summary(col_smote_int_sq)
